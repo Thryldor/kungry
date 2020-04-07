11fa0303c095f3f896efabf38a898fd9e7dca65a
Philippe <philippe.desousaviolante@gmail.com>
2020-04-07 12:18:29 -0400

    Cleanup after merge

--- app/src/main/java/ca/ulaval/ima/mp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/ca/ulaval/ima/mp/ui/profile/ProfileFragment.kt
@@ -23,7 +23,7 @@ class ProfileFragment : Fragment() {
     ): View? {
         profileViewModel =
             ViewModelProviders.of(this).get(ProfileViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_login, container, false)
+        val root = inflater.inflate(R.layout.profile_fragment, container, false)
         val textView: TextView = root.findViewById(R.id.text_notifications)
         textView.setText("This is the login page")
 
